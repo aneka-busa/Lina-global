f54ffc2191f0c015ee2e1597d7bcc6a9a926d87a
aneka-busa <anekabusa3@gmail.com>
2022-05-24 13:33:51 +0700

    Ok

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,7 +27,7 @@ class IndexPage extends Component {
         />
           <center>
           
-            <h3>Amanita</h3>
+            <h3>Maspupah</h3>
             <p><ins>AGEN RESMI PT ANEKA BUSA INDONESIA</ins></p>
             <br/>
 
