54d70c99feca149d06cfc55af80d44ff454fae3e
aneka-busa <anekabusa3@gmail.com>
2022-05-24 14:28:28 +0700

    ok

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,7 +27,7 @@ class IndexPage extends Component {
         />
           <center>
           
-            <h3> Teguh Sriyanto</h3>
+            <h3>Saman</h3>
             <p><ins>AGEN RESMI PT ANEKA BUSA INDONESIA</ins></p>
             <br/>
 
