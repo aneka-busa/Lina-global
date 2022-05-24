14c2e145877d199745ee14906949cf83fcebf34b
aneka-busa <anekabusa3@gmail.com>
2022-05-24 15:01:31 +0700

    Ok

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,7 +27,7 @@ class IndexPage extends Component {
         />
           <center>
           
-            <h3>Saman</h3>
+            <h3> Akhmad Tijani</h3>
             <p><ins>AGEN RESMI PT ANEKA BUSA INDONESIA</ins></p>
             <br/>
 
