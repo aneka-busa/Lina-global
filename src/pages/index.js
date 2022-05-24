14af3640f2177bfe4632e78f64530ea473fa9a4a
aneka-busa <anekabusa3@gmail.com>
2022-05-24 16:20:09 +0700

    ok

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,7 +27,7 @@ class IndexPage extends Component {
         />
           <center>
           
-            <h3> Ahmad Arwani</h3>
+            <h3> Hasuroh</h3>
             <p><ins>AGEN RESMI PT ANEKA BUSA INDONESIA</ins></p>
             <br/>
 
