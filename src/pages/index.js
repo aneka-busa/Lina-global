2d083a1ff3a51f03446d8422b1bf0d64967c43f5
aneka-busa <anekabusa3@gmail.com>
2022-05-24 10:36:48 +0700

    ok

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,7 +27,7 @@ class IndexPage extends Component {
         />
           <center>
           
-            <h3>Dedi Rustandi</h3>
+            <h3>Widikdo</h3>
             <p><ins>AGEN RESMI PT ANEKA BUSA INDONESIA</ins></p>
             <br/>
 
