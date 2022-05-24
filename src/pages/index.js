28954a5d20be505c07a823a761563b86f5b1cb0c
aneka-busa <anekabusa3@gmail.com>
2022-05-24 09:08:11 +0700

    ok

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,7 +27,7 @@ class IndexPage extends Component {
         />
           <center>
           
-            <h3>Nani Suprapti</h3>
+            <h3>Normala Sari Pane</h3>
             <p><ins>AGEN RESMI PT ANEKA BUSA INDONESIA</ins></p>
             <br/>
 
