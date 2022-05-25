ce13f5b55c5bb8e3fa235de2711821f9642250aa
aneka-busa <anekabusa3@gmail.com>
2022-05-25 14:25:02 +0700

    ok

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,7 +27,7 @@ class IndexPage extends Component {
         />
           <center>
           
-            <h3>Wijianto</h3>
+            <h3>Mumu Nugraha</h3>
             <p><ins>AGEN RESMI PT ANEKA BUSA INDONESIA</ins></p>
             <br/>
 
