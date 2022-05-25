2740fbadb01a7eb6a9823f05852850af537e62e4
aneka-busa <anekabusa3@gmail.com>
2022-05-25 14:46:08 +0700

    ok

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,7 +27,7 @@ class IndexPage extends Component {
         />
           <center>
           
-            <h3>Wilastri</h3>
+            <h3>Lili Suheri</h3>
             <p><ins>AGEN RESMI PT ANEKA BUSA INDONESIA</ins></p>
             <br/>
 
