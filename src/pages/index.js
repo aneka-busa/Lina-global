ad814652beb5b80530f026cf1ef1329c79e8afac
aneka-busa <anekabusa3@gmail.com>
2022-05-25 10:59:14 +0700

    ok

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,7 +27,7 @@ class IndexPage extends Component {
         />
           <center>
           
-            <h3>Ahmad Martinus</h3>
+            <h3>Iman Sugiono</h3>
             <p><ins>AGEN RESMI PT ANEKA BUSA INDONESIA</ins></p>
             <br/>
 
