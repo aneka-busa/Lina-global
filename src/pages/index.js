f9a7a2c76ac84f76ecadcf95a011bb4423247aae
aneka-busa <anekabusa3@gmail.com>
2022-05-25 15:09:03 +0700

    ok

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,7 +27,7 @@ class IndexPage extends Component {
         />
           <center>
           
-            <h3>Hidayat Addahlani</h3>
+            <h3>Candra Hariyanto</h3>
             <p><ins>AGEN RESMI PT ANEKA BUSA INDONESIA</ins></p>
             <br/>
 
