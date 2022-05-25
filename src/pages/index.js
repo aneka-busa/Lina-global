238ee2d591b3ee86d9bce4a40c3d00419fbc83b5
aneka-busa <anekabusa3@gmail.com>
2022-05-25 14:07:31 +0700

    ok

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,7 +27,7 @@ class IndexPage extends Component {
         />
           <center>
           
-            <h3> Rika</h3>
+            <h3>Samsul Rijal</h3>
             <p><ins>AGEN RESMI PT ANEKA BUSA INDONESIA</ins></p>
             <br/>
 
