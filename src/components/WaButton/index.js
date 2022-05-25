f2f0470624eb21f523e840d3f516cc4159ffcf90
aneka-busa <anekabusa3@gmail.com>
2022-05-25 11:21:14 +0700

    ok

--- src/components/WaButton/index.js
+++ src/components/WaButton/index.js
@@ -5,7 +5,7 @@ class WaButton extends Component {
   render() {
     return <a className="wa-button-wrapper" 
     target="_blank" 
-    href="https://api.whatsapp.com/send?phone= 6285352821180&text=Saya%20tertarik%20untuk%20membeli%20produk%20ini%21%0A%0ANama%20%3A%0ANama_Produk%20%3A%20%0AKode_Barang%20%3A%20%0AMotif%20%3A%20%0AAlamat_lengkap%20%3A%20%0A"
+    href="https://api.whatsapp.com/send?phone= 6281311006993&text=Saya%20tertarik%20untuk%20membeli%20produk%20ini%21%0A%0ANama%20%3A%0ANama_Produk%20%3A%20%0AKode_Barang%20%3A%20%0AMotif%20%3A%20%0AAlamat_lengkap%20%3A%20%0A"
     rel="noopener noreferrer">
       <img src={wa} alt=""  width="60px"/>
     </a>
