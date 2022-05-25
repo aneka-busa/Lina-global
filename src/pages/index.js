1bfa8fbc570e1ce47777650a36bbeaed5004aea4
aneka-busa <anekabusa3@gmail.com>
2022-05-25 14:01:19 +0700

    ok

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,7 +27,7 @@ class IndexPage extends Component {
         />
           <center>
           
-            <h3>Ratna</h3>
+            <h3> Rika</h3>
             <p><ins>AGEN RESMI PT ANEKA BUSA INDONESIA</ins></p>
             <br/>
 
