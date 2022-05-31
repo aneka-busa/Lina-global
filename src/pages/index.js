701f591e7b0af9709747b8eee25a7416c9b05496
aneka-busa <anekabusa3@gmail.com>
2022-05-31 14:58:49 +0700

    ok

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,7 +27,7 @@ class IndexPage extends Component {
         />
           <center>
           
-            <h3>Ismail</h3>
+            <h3>Sri Mustikawati</h3>
             <p><ins>AGEN RESMI PT ANEKA BUSA INDONESIA</ins></p>
             <br/>
 
