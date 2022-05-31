02a17b50d20afbe50a651c63992daff66c0eeddc
aneka-busa <anekabusa3@gmail.com>
2022-05-31 16:06:42 +0700

    ok

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,7 +27,7 @@ class IndexPage extends Component {
         />
           <center>
           
-            <h3>Sutrisna</h3>
+            <h3>Bbeben Burhan</h3>
             <p><ins>AGEN RESMI PT ANEKA BUSA INDONESIA</ins></p>
             <br/>
 
