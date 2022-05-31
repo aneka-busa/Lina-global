562ca4f88fdeae15430f5a308c53d06c96f91320
aneka-busa <anekabusa3@gmail.com>
2022-05-31 11:45:31 +0700

    ok

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,7 +27,7 @@ class IndexPage extends Component {
         />
           <center>
           
-            <h3> Nur Laila Najmi</h3>
+            <h3>Yanti</h3>
             <p><ins>AGEN RESMI PT ANEKA BUSA INDONESIA</ins></p>
             <br/>
 
