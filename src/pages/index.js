26ab13f0ab6980a8521e2310c2ded952aaa3dd37
aneka-busa <anekabusa3@gmail.com>
2022-05-31 14:45:53 +0700

    ok

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,7 +27,7 @@ class IndexPage extends Component {
         />
           <center>
           
-            <h3>Soni fela</h3>
+            <h3>Ismail</h3>
             <p><ins>AGEN RESMI PT ANEKA BUSA INDONESIA</ins></p>
             <br/>
 
