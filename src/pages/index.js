d15ff6bbb04404f2da5ef9056625d2f17f6ed0eb
aneka-busa <anekabusa3@gmail.com>
2022-06-02 09:00:23 +0700

    ok

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,7 +27,7 @@ class IndexPage extends Component {
         />
           <center>
           
-            <h3>Jam Sriyono</h3>
+            <h3>Lina</h3>
             <p><ins>AGEN RESMI PT ANEKA BUSA INDONESIA</ins></p>
             <br/>
 
